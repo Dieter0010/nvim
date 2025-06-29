d69e775891a2ab2da2bfb0f060862e22042a48ec
Dieter0010 <dieterzankel07@gmail.com>
2025-06-29 15:35:26 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,22 @@
+vim.cmd("let mapleader = ' '")
+local vimrc = vim.fn.stdpath("config") .. "/.vimrc"
+vim.cmd.source(vimrc)
+
+require("preferences")
+require("installing-lazy")
+require("keymaps")
+require("icons").setup()
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+	nested = true,
+
+	callback = function()
+		vim.cmd.colorscheme(vim.g.SCHEME)
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+	callback = function(params)
+		vim.g.SCHEME = params.match
+	end,
+})
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+-- return {
+-- 	{ "gbprod/nord.nvim" },
+--
+-- 	{
+-- 		"folke/tokyonight.nvim",
+-- 		lazy = false,
+-- 		priority = 1000,
+-- 		opts = {},
+-- 	},
+-- }
+return {
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+        transparent = true,
+      })
+      vim.cmd.colorscheme("tokyonight-night")
+    end,
+  },
+}
