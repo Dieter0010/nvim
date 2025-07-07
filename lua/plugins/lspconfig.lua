7c9c61c6c55e089ac59f6689bf811d5c96f6211d
Dieter0010 <dieterzankel07@gmail.com>
2025-07-07 18:37:35 +0200

    added pyright

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -12,6 +12,7 @@ return {
 
 		config = function()
 			local lspconfig = require("lspconfig")
+			lspconfig.pyright.setup {}
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local border = {
 				{ "🭽", "FloatBorder" },
