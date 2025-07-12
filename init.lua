b70bd34fc9ae5b6ae91e13e982a910f00f9c982b
Dieter0010 <dieterzankel07@gmail.com>
2025-07-12 21:43:33 +0200

    Update init.lua
    Removed excessive vimrc sourcing

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 vim.cmd("let mapleader = ' '")
-local vimrc = vim.fn.stdpath("config") .. "/.vimrc"
-vim.cmd.source(vimrc)
+-- local vimrc = vim.fn.stdpath("config") .. "/.vimrc"
+-- vim.cmd.source(vimrc)
 
 require("preferences")
 require("installing-lazy")
